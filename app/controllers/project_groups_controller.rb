b3c2e20d42149f3ed50ca56e5b5c2c181e6b00c4
Jan Vlnas <git@jan.vlnas.cz>
2012-04-15 21:56:29 +0200

    Prepare ProjectGroupsController

--- app/controllers/project_groups_controller.rb
+++ app/controllers/project_groups_controller.rb
@@ -4,19 +4,15 @@ class ProjectGroupsController < ApplicationController
   before_filter :find_project_by_project_id #, :only => :new
   #before_filter :find_model_object, :except => :new
   #before_filter :find_project_from_association, :except => :new
-  #before_filter :authorize
+  before_filter :authorize
 
 
   def show
     @project_group = ProjectGroup.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @project_group }
-    end
   end
 
-  def new
+  def edit
+    @project_group = ProjectGroup.find(params[:id])
   end
 
   def create
--- app/helpers/project_groups_helper.rb
+++ app/helpers/project_groups_helper.rb
@@ -1,2 +1,13 @@
 module ProjectGroupsHelper
+
+  # Generate link to a given project's settings
+  # @param project [Project]
+  # @param label [Symbol]
+  # @param tab [String]
+  def link_to_project_settings(project, label = :label_settings, tab = "project_groups")
+
+    link_to l(:label_settings), :controller => "projects", :action => "settings", :id => project, :tab => tab
+
+  end
+
 end
