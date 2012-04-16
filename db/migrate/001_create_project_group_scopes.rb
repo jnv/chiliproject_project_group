8dd1e4cb50d1cd4ca03f1e16c663d21df871664f
Jan Vlnas <git@jan.vlnas.cz>
2012-04-16 13:57:17 +0200

    Add ID column to join table Either PG gem or ObjectDaddy always tries to return id: `INSERT INTO "project_group_scopes" ("project_id", "project_group_id", "manageable") VALUES(2, 4, 'f') RETURNING "id"`

--- db/migrate/001_create_project_group_scopes.rb
+++ db/migrate/001_create_project_group_scopes.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class CreateProjectGroupScopes < ActiveRecord::Migration
   def self.up
-    create_table :project_group_scopes, :id => false do |t|
+    create_table :project_group_scopes, :id => true do |t| #XXX There should be no ID column but Postgres w/ ObjectDaddy don't like it
       t.column :project_id, :integer, :null => false
       t.column :project_group_id, :integer, :null => false
       t.column :manageable, :boolean, :default => false, :null => false
