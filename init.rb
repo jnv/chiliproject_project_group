f1d0e9eacde46a01c02302cf5d18b2243967cc84
Jan Vlnas <git@jan.vlnas.cz>
2012-09-11 15:06:39 +0200

    Bump version

--- init.rb
+++ init.rb
@@ -43,7 +43,7 @@ Redmine::Plugin.register :chiliproject_project_group do
   name 'Project Groups'
   author 'Jan Vlnas'
   description 'Provides per-project user groups'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/jnv/chiliproject_project_group'
   author_url 'https://github.com/jnv'
 
